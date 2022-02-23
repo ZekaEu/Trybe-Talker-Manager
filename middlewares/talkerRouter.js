8d80ce4004fa076c0e12c31de8c34fc93caf0bf6
ZekaEu <zeka.surf@hotmail.com>
2022-02-23 08:15:06 -0300

    Requisito 2

--- middlewares/talkerRouter.js
+++ middlewares/talkerRouter.js
@@ -3,10 +3,21 @@ const { readFileSync } = require('fs');
 
 const talkerRouter = express.Router();
 
+const getTalkerArr = () => JSON.parse(readFileSync('talker.json'));
+
 talkerRouter.get('/', (_req, res) => {
-  const talkerArr = JSON.parse(readFileSync('talker.json'));
-  if (!talkerArr.length) return res.status(200).send(talkerArr);
-  res.status(200).json(talkerArr);
+  if (!getTalkerArr().length) return res.status(200).send(getTalkerArr());
+  res.status(200).json(getTalkerArr());
+});
+
+talkerRouter.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const talkerById = getTalkerArr()
+    .find((t) => t.id === parseInt(id, 10));
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talkerById);
 });
 
 module.exports = talkerRouter;
